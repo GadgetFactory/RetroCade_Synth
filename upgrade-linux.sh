433560f993d8ab3b7e0c988d0e6e703893d659d0
user <user@ubuntu.(none)>
2013-10-01 12:24:18 -0400

    Installer for linux

---
+++ upgrade-linux.sh
@@ -0,0 +1,3 @@
+#/bin/bash
+# This will upgrade your Papilio Pro LX9 RetroCade to the latest bit file.
+sudo Installer/lin32/papilio-prog -b Installer/lin32/bscan_spi_xc6slx9.bit -f ZPUino_SOC/RetroCade-1.1-zpuino-1.0-PapilioPro-S6LX9-RetroCade-1.1.bit
